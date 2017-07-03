6e6c238196afa646aae7270c45c22a641a7a0bd4
yandong <ereddate@gmail.com>
2017-07-03 14:38:43 +0800

    更新pages项目demo
    更新pages项目demo

--- src/pages/js/app.0.0.1.js
+++ src/pages/js/app.0.0.1.js
@@ -1,6 +1,6 @@
 $.setBaseFontSize(16);
 var callbacks = $.Callbacks();
 
-{{ require("router.script main header footer nav page") }}
+{{ require("router.script main header footer nav page dialog") }}
 
 callbacks.done();
