8fc55b3b798ab239ccdbf46e785442a4dc8d8dd5
yandong <ereddate@gmail.com>
2017-07-11 11:31:23 +0800

    更新hackernews的demo
    更新hackernews的demo

--- src/hackernews/js/app.0.0.1.js
+++ src/hackernews/js/app.0.0.1.js
@@ -1,6 +1,6 @@
 $.setBaseFontSize(16);
 var callbacks = $.Callbacks();
 
-{{ require("common loading router.script header login footer nav list dialogheader comment_list comment_input comment usinfo main") }}
+{{ require("common loading router.script header login footer nav list dialogheader comment_list comment_input comment usinfo article main") }}
 
 callbacks.done();
