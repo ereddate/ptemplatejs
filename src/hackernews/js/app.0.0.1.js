075a4fe71950a9b45c759d9e47855bcae7924e7a
yandong <ereddate@gmail.com>
2017-07-31 15:05:56 +0800

    更新hackernews的demo,增加3dtouch效果
    更新hackernews的demo,增加3dtouch效果

--- src/hackernews/js/app.0.0.1.js
+++ src/hackernews/js/app.0.0.1.js
@@ -4,7 +4,7 @@ var callbacks = $.Callbacks();
 import {api, apiB} from "api";
 console.log(api, apiB);
 
-import "common adinclude loading dialog router.script follow gotop dialogheader";
+import "common adinclude loading dialog router.script follow gotop dialogheader touchmenu";
 import "comment usinfo article teacher";
 import "main";
 
