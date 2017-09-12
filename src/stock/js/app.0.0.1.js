b3d90f91b4aa3164cf9e8d96df0d11f3bcac7fd2
yandong <ereddate@gmail.com>
2017-09-12 09:16:39 +0800

    add project stock

---
+++ src/stock/js/app.0.0.1.js
@@ -0,0 +1,58 @@
+$.setBaseFontSize(16);
+
+import {get, set} from "common.script";
+
+var type = get.getURLKeyValue(location.href, "type");
+var app_type = type || "web";
+import "face";
+
+var callbacks = new $.Callbacks();
+if (app_type != "web") {
+	callbacks.add(function(next) {
+		var b = 3;
+		var face = $.render("my-face", {
+			content: "pTemplateJs Demo",
+			ad: "#",
+			adurl: "#",
+			adtitle: "ad",
+			num: b
+		}, $.createDom("div", {}), function(e) {
+			$.query(".face")[0] && $.query(".face")[0]._remove();
+			$.query("body")[0]._append(e.children[0]);
+		});
+		face.interval = setInterval(function() {
+			b -= 1;
+			if (b <= 0) {
+				face.interval && clearInterval(face.interval);
+				$.query(".face")[0] && $.query(".face")[0]._remove();
+				return;
+			}
+			$.update("my-face", {
+				num: b
+			});
+		}, 1000);
+		next();
+	});
+}
+
+var time = get.time,
+	cookie = get.cookie,
+	getData = get.getData,
+	isSignIn = get.isSignIn,
+	cid = get.getcid();
+
+var signIn = isSignIn();
+console.log(signIn);
+
+var q = get.getURLKeyValue(location.href, "channel"),
+	g = get.getURLKeyValue(location.href, "grail_type"),
+	teachid = get.getURLKeyValue(location.href, "id"),
+	teachurl = get.getURLKeyValue(location.href, "url"),
+	starchannel = get.getURLKeyValue(location.href, "star");
+console.log(q);
+
+import "common.style store";
+import "common_router.script news_router.script market_router.script star_router.script search_router.script famousman_router.script";
+import "header footer gotop loading dialog";
+import "main";
+import "track";
