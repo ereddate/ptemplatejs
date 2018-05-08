f1ed1414d5110f1f12922e0e92eba254c436b5d8
yandong <ereddate@gmail.com>
2018-05-08 15:27:52 +0800

    Create ptemplate.extend.pt.js

---
+++ ptemplate.extend.pt.js
@@ -0,0 +1,53 @@
+/*!
+ * ptemplatejs v1.0.0
+ * @author yandong
+ *
+ * https://github.com/ereddate/ptemplatejs
+
+	PT({
+		router: {
+			"/test": function(e, args) {
+				console.log(args.title);
+			}
+		},
+		store: {
+			state: {
+				api: {
+					getdata: "/"
+				}
+			},
+			props: {
+				title: "test"
+			}
+		},
+		ready: function(data) {
+			$.render("my-main", data.props, $.query("#app"))
+		}
+	})
+
+
+ */
+ 'use strict';
+typeof window.pTemplate != "undefined" && (function(win, $) {
+
+	var pt = function(options){
+		return new pt.fn.init(options);
+	};
+	pt.fn = pt.prototype = {
+		init: function(options){
+			$.extend(this, options);
+			$.router(this.router);
+			var store = $.store("PT_store", {});
+			store.commit(this.store);
+			var then = this;
+			typeof this.ready !== "undefined" && $.ready(function(){
+				then.ready(store.get());
+			});
+			return this;
+		}
+	};
+	pt.fn.init.prototype = pt.fn;
+
+	win.PT = pt;
+
+})(window, pTemplate);
