761029cde8f519ce3404e1d906657f241baf2e79
yandong <ereddate@gmail.com>
2018-12-20 10:51:30 +0800

    识别PJSX模板中的STYLE标签，并可以用@lessParse方法对style标签的less代码转为css
    识别PJSX模板中的STYLE标签，并可以用@lessParse方法对style标签的less代码转为css

--- bin/pjsx-build.js
+++ bin/pjsx-build.js
@@ -1,17 +1,47 @@
+var less = require("less"),
+	cleanCSS = require('clean-css'),
+	parseLessCount = 0,
+	parseStyleCount = 0,
+	parseSingleElemCount = 0,
+	parsePairElemCount = 0;
+
+function parseLess(c) {
+	less.render(c, function(e, output) {
+		if (e) {
+			console.log(e);
+		}
+		var text = output.css;
+		text = (new cleanCSS({}).minify(text)).styles;
+		c = "<style>" + text + "</style>";
+	});
+	return c;
+}
 module.exports = {
 	parsePJSX: function(result, callback) {
 		var doms = "a|div|p|span|h1|h2|h3|h4|h5|h6|article|aside|address|ul|li|header|footer|ol|menu|data|datalist|dd|dt|dl|form|input|textarea|label|nav|meta|link|script|select|option|section|strong|style|table|tbody|th|tr|td|caption|tfoot|thead|title|video|audio|img|em|button|template|textnode",
-			reg = new RegExp("((<\\\s*(" + doms + ")\\\s*[^<>]*\\\s*>|)(\\\s*[\\\r\\\n\\\t]*\\\s*|)(<\\\s*(" + doms + ")\\\s*[^<>]*\\\s*>|<\\\s*(" + doms + ")\\\s*[^<>]*\\\s*\\\/\\\s*>)(\\\s*[\\\r\\\n\\\t]*\\\s*.*\\\s*[\\\r\\\n\\\t]*\\\s*<\\\s*\\\/\\\s*(" + doms + ")\\\s*>)*(\\\s*[\\\r\\\n\\\t]*\\\s*|)(<\\\s*\\\/\\\s*(" + doms + ")\\\s*[^<>]*\\\s*>|))+", "gim");
+			reg = new RegExp("((\\\@lessParse\\\(|\\\(|)<\\\s*style\\\s*>\\\s*[^<>]*\\\s*<\\\s*\\\/style\\\s*>\\\)*|(<\\\s*(" + doms + ")\\\s*[^<>]*\\\s*>|)(\\\s*[\\\r\\\n\\\t]*\\\s*|)(<\\\s*(" + doms + ")\\\s*[^<>]*\\\s*>|<\\\s*(" + doms + ")\\\s*[^<>]*\\\s*\\\/\\\s*>)(\\\s*[\\\r\\\n\\\t]*\\\s*.*\\\s*[\\\r\\\n\\\t]*\\\s*<\\\s*\\\/\\\s*(" + doms + ")\\\s*>)*(\\\s*[\\\r\\\n\\\t]*\\\s*|)(<\\\s*\\\/\\\s*(" + doms + ")\\\s*[^<>]*\\\s*>|))+", "gim");
 		result = result.replace(/"([^"]+)\s+>\s+([^"]+)"/gim, "\"$1 $more $2\"").replace(/"([^"]+)\s+<\s+([^"]+)"/gim, "\"$1 $less $2\"");
 		result = result.replace(reg, function(a, b) {
 			//console.log("-> ", a)
+			if (/\@lessParse\(<\s*style\s*>\s*[^<>]*\s*<\s*\/style\s*>\)*/.test(a)) {
+				a = a.replace(/\@lessParse\(<\s*style\s*>\s*([^<>]*)\s*<\s*\/style\s*>\)*/gim, function(b, c) {
+					parseLessCount += 1;
+					return parseLess(c);
+				});
+			} else if (/\(<\s*style\s*>\s*[^<>]*\s*<\s*\/style\s*>\)/.test(a)) {
+				parseStyleCount += 1;
+				a = a.replace(/^\(/gim, "").replace(/\)$/gim, "");
+			}
 			if (/((\s*[\r\n\t]*\s*|)<\s*(area|br|col|embed|hr|img|input|link|meta|param)\s*[^<>]*\s*\/\s*>(\s*[\r\n\t]*\s*|))+/.test(a)) {
+				parseSingleElemCount+=1;
 				a = a.replace(a, " $.DOM.div({html: '" + a.replace(/[\r\n\t]*/gim, "").replace(/([<>]+)\s*/gim, "$1").replace(/'/gim, "\\\'").replace(/"/gim, "\\\"") + "'})");
 			} else {
+				parsePairElemCount+=1;
 				a = a.replace(a, " $.DOM.div({html: '" + a.replace(/[\r\n\t]*/gim, "").replace(/([<>]+)\s*/gim, "$1").replace(/'/gim, "\\\'").replace(/"/gim, "\\\"") + "'})._children()[0]");
 			}
 			return a;
 		});
+		console.log("[parsed] ".green, "less.".yellow + parseLessCount, "style.".yellow + parseStyleCount, "singleElem.".yellow + parseSingleElemCount, "pairElem.".yellow + parsePairElemCount);
 		result = result.replace(/"([^"]+)\s+\$more\s+([^"]+)"/gim, "\"$1 > $2\"").replace(/"([^"]+)\s+\$less\s+([^"]+)"/gim, "\"$1 < $2\"");
 		return result;
 	}
